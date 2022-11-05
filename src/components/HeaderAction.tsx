d329f175da6b39f0e43838b4a5c920990e40b17a
Samuel Blebo <samuel.blebo@yahoo.com>
2022-11-05 20:41:13 +0000

    link added to button but not working as expected

--- src/components/HeaderAction.tsx
+++ src/components/HeaderAction.tsx
@@ -137,7 +137,13 @@ export function HeaderAction({ links }: HeaderActionProps) {
         <Group spacing={5} className={classes.links}>
           {items}
         </Group>
-        <Button radius="md" color="yellow.5" sx={{ height: 30 }} uppercase>
+        <Button
+          component="a"
+          radius="md"
+          color="yellow.5"
+          sx={{ height: 30 }}
+          href="https://twitter.com/mantinedev"
+        >
           GET IN TOUCH
         </Button>
       </Container>
